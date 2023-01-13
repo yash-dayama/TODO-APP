f4e837c57de76fe8e1832b74269b6bcd8d88e6e0
Yash Patel <yashkumarhpatel19@gnu.ac.in>
2023-01-13 16:17:24 +0530

    Day5

--- lib/Ui/notified_page.dart
+++ lib/Ui/notified_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NotifiedPage extends StatelessWidget {
   final String? label;
@@ -30,14 +32,14 @@ class NotifiedPage extends StatelessWidget {
               Container(
                 child: Text("Hello",
                     style: TextStyle(
-                        color: Colors.black,
+                        color: Get.isDarkMode ? Colors.white : Colors.black,
                         fontSize: 30,
                         fontWeight: FontWeight.bold)),
               ),
               Container(
                 child: Text("You Have a new Reminder!",
                     style: TextStyle(
-                      color: Colors.black,
+                      color: Get.isDarkMode ? Colors.white : Colors.black,
                       fontSize: 20,
                     )),
               ),
@@ -49,14 +51,79 @@ class NotifiedPage extends StatelessWidget {
                   width: 300,
                   decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(20),
-                      color: Get.isDarkMode ? Colors.white : Colors.blue[800]),
-                  child: Center(
-                    child: Text(
-                      this.label.toString().split("|")[1],
-                      style: TextStyle(
-                          color: Get.isDarkMode ? Colors.black : Colors.white,
-                          fontSize: 30),
-                    ),
+                      color: Get.isDarkMode ? Colors.grey : Colors.blue[800]),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 50,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.text_format,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                          Text("Title",
+                              style: GoogleFonts.lato(
+                                  textStyle: TextStyle(
+                                      fontSize: 20, color: Colors.grey[100]))),
+                        ],
+                      ),
+                      Text(
+                        this.label.toString().split("|")[0],
+                        style: TextStyle(
+                            color: Get.isDarkMode ? Colors.black : Colors.white,
+                            fontSize: 30),
+                      ),
+                      SizedBox(
+                        height: 60,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.description,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                          Text("Description",
+                              style: GoogleFonts.lato(
+                                  textStyle: TextStyle(
+                                      fontSize: 20, color: Colors.grey[100]))),
+                        ],
+                      ),
+                      Text(
+                        this.label.toString().split("|")[1],
+                        style: TextStyle(
+                            color: Get.isDarkMode ? Colors.black : Colors.white,
+                            fontSize: 25),
+                      ),
+                      SizedBox(
+                        height: 60,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.access_time_rounded,
+                            color: Colors.grey[200],
+                            size: 20,
+                          ),
+                          Text("Time",
+                              style: GoogleFonts.lato(
+                                  textStyle: TextStyle(
+                                      fontSize: 20, color: Colors.grey[100]))),
+                        ],
+                      ),
+                      Text(
+                        this.label.toString().split("|")[2],
+                        style: TextStyle(
+                            color: Get.isDarkMode ? Colors.black : Colors.white,
+                            fontSize: 25),
+                      ),
+                    ],
                   )),
             ],
           ),
